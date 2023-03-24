26776ec9ed9cb76c6772d697355187bb0ab1775a
Denis Skurat <denis.skurat@effective-soft.com>
2023-03-24 19:35:54 +0300

    Add UserEntity

---
+++ src/Services/Users/Core/Users.Domain/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Users.Domain.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string? Email { get; set; } = null;
+    }
+}

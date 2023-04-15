d8d7de60e8eba7a63ec26ada80d2af9fe3854af8
Денис Скурат <den.skur2016@yandex.ru>
2023-04-15 15:20:10 +0300

    Add additional properties for products

--- src/Services/Products/Api/Products.Api/Models/Products/Request/PutProductRequest.cs
+++ src/Services/Products/Api/Products.Api/Models/Products/Request/PutProductRequest.cs
@@ -15,5 +15,18 @@ namespace Products.Api.Models.Products.Request
         [BindRequired]
         [Required]
         public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("description")]
+        [BindRequired]
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("phone_numbers")]
+        [BindRequired]
+        [Required]
+        public List<string> PhoneNumbers { get; set; } = new List<string>();
+
+        [JsonPropertyName("price")]
+        public decimal? Price { get; set; }
     }
 }
--- src/Services/Products/Core/Products.Domain/Entities/ProductEntity.cs
+++ src/Services/Products/Core/Products.Domain/Entities/ProductEntity.cs
@@ -6,6 +6,10 @@
 
         public string Name { get; set; } = string.Empty;
 
+        public string Description { get; set; } = string.Empty;
+        
+        public List<string> PhoneNumbers { get; set; } = new List<string>();
+
         public decimal? Price { get; set; }
     }
 }
